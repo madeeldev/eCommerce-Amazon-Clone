214c0ad33554af1ec826e317379c038c76940f0b
Adeel <ranaadeelofficial@gmail.com>
2019-10-23 05:52:44 +0500

    main layout
    main layout

--- app/src/main/java/adeelsafdar/ecommerce/MainActivity.java
+++ app/src/main/java/adeelsafdar/ecommerce/MainActivity.java
@@ -3,12 +3,20 @@ package adeelsafdar.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mJoinNowButton, mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mJoinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        mLoginButton = (Button) findViewById(R.id.main_login_btn);
+        
+
     }
 }
